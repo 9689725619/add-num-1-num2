9ded502ccf2989fde420007f56bd7b54a7560c01
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-03-29 17:48:51 +0530

    Add files via upload

---
+++ add num1,num2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,res;
+	
+	num1=55;
+	num2=66;
+	res=num1+num2;
+	printf("Ener two number");
+	scanf("%d,%d",&num1,&num2);
+	{
+	
+	printf("%d",res);
+}
+	return 0;
+	
+}
